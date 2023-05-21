aa10d11067644f61bcd81ab9c5e5bee9bee22484
Sagar Sharma <tech.sagarsh@gmail.com>
2023-05-22 01:13:54 +0530

    fixed the nav setting popup

--- src/components/Auth/SignIn/index.js
+++ src/components/Auth/SignIn/index.js
@@ -1,124 +1,133 @@
-import { SignUpLink } from '../SignUp'
-import { ForgetPasswordLink } from '../PasswordForget'
-import { useState } from 'react'
-import { withFirebase } from '../../Firebase'
-import { useNavigate } from 'react-router-dom'
-import * as ROUTES from '../../../constants/routes'
-import { FcGoogle } from 'react-icons/fc'
-import { FaGithub } from 'react-icons/fa'
-
+import { SignUpLink } from "../SignUp";
+import { ForgetPasswordLink } from "../PasswordForget";
+import { useState } from "react";
+import { withFirebase } from "../../Firebase";
+import { useNavigate } from "react-router-dom";
+import * as ROUTES from "../../../constants/routes";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 const SignInPage = () => (
-    <div className='sign-in-container'>
-        <h1>Sign In</h1>
-        <SignInForm />
-        <h4>Or</h4>
-        <SignInWithGoogle />
-        <SignInWithGithub />
-        <ForgetPasswordLink />
-        <SignUpLink />
-    </div>
-)
+  <div className="sign-in-container">
+    <h1>Sign In</h1>
+    <SignInForm />
+    <h4>Or</h4>
+    <SignInWithGoogle />
+    <SignInWithGithub />
+    <ForgetPasswordLink />
+    <SignUpLink />
+  </div>
+);
 
 const SignInFormBase = (props) => {
-    const [userDetail, setUserDetail] = useState({
-        email: '',
-        password: '',
-        error: null
-    })
+  const [userDetail, setUserDetail] = useState({
+    email: "",
+    password: "",
+    error: null,
+  });
 
-    const { email, password, error } = userDetail
-    const navigateProgrammatically = useNavigate()
+  const { email, password, error } = userDetail;
+  const navigateProgrammatically = useNavigate();
 
-    function handleDetailsChange(e) {
-        setUserDetail(prevDetail => ({ ...prevDetail, [e.target.name]: e.target.value }))
-    }
-    function handleSignIn(e) {
-        props.firebase.doSignInWithEmailAndPassword(email, password)
-            .then(data => {
-                setUserDetail({
-                    email: '',
-                    password: '',
-                    error: null
-                })
-                navigateProgrammatically(ROUTES.HOME)
-            })
-            .catch(error => {
-                setUserDetail(prevDetail => ({ ...prevDetail, error: error }))
-            })
-        e.preventDefault()
-    }
-    const invalidSignIn =
-        email === '' ||
-        password === ''
+  function handleDetailsChange(e) {
+    setUserDetail((prevDetail) => ({
+      ...prevDetail,
+      [e.target.name]: e.target.value,
+    }));
+  }
+  function handleSignIn(e) {
+    props.firebase
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        setUserDetail({
+          email: "",
+          password: "",
+          error: null,
+        });
+      })
+      .then(() => {
+        navigateProgrammatically(ROUTES.HOME);
+      })
+      .catch((error) => {
+        setUserDetail((prevDetail) => ({ ...prevDetail, error: error }));
+      });
+    e.preventDefault();
+  }
+  const invalidSignIn = email === "" || password === "";
 
-    return (
-        <div>
-            <form className='sign-in-form'>
-                <input
-                    type="email"
-                    name="email"
-                    placeholder='Email Address'
-                    onChange={handleDetailsChange}
-                    value={email} />
+  return (
+    <div>
+      <form className="sign-in-form">
+        <input
+          type="email"
+          name="email"
+          placeholder="Email Address"
+          onChange={handleDetailsChange}
+          value={email}
+        />
 
-                <input type="password"
-                    name="password"
-                    placeholder='Password'
-                    onChange={handleDetailsChange}
-                    value={password} />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleDetailsChange}
+          value={password}
+        />
 
-                <button disabled={invalidSignIn} onClick={handleSignIn}>Sign In</button>
+        <button disabled={invalidSignIn} onClick={handleSignIn}>
+          Sign In
+        </button>
 
-                {error && <p>{error.message}</p>}
-            </form>
-        </div>
-    )
-}
+        {error && <p>{error.message}</p>}
+      </form>
+    </div>
+  );
+};
 
 const SignInWithGoogleBase = (props) => {
-    const navigateProgrammatically = useNavigate()
+  const navigateProgrammatically = useNavigate();
 
-    function handleSignIn() {
-        props.firebase.signInWithGoogle()
-        navigateProgrammatically(ROUTES.HOME)
-    }
-    return (
-        <button
-            className='popup-sign-in-btn google-popup-sign-in-btn '
-            onClick={handleSignIn}>
-            <FcGoogle className='icon' />
-            <p>Continue with Google</p>
-        </button>
-    )
-}
+  function handleSignIn() {
+    props.firebase.signInWithGoogle();
+    navigateProgrammatically(ROUTES.HOME);
+  }
+  return (
+    <button
+      className="popup-sign-in-btn google-popup-sign-in-btn "
+      onClick={handleSignIn}
+    >
+      <FcGoogle className="icon" />
+      <p>Continue with Google</p>
+    </button>
+  );
+};
 
 const SignInWithGithubBase = (props) => {
-    const navigateProgrammatically = useNavigate()
+  const navigateProgrammatically = useNavigate();
 
-    async function handleSignIn() {
-        try {
-            const result = await props.firebase.signInWithGithub()
-            console.log('Signin with Github Successful...')
-            props.firebase.auth["allowWithoutEmailVerification"] = true
-            navigateProgrammatically(ROUTES.HOME)
-        }
-        catch(err) {
-            console.log("Couldn't Signin", err)
-        }
+  async function handleSignIn() {
+    try {
+      const result = await props.firebase.signInWithGithub();
+      console.log("Signin with Github Successful...");
+      props.firebase.auth["allowWithoutEmailVerification"] = true;
+      navigateProgrammatically(ROUTES.HOME);
+    } catch (err) {
+      console.log("Couldn't Signin", err);
     }
-    return (
-        <button
-            className='popup-sign-in-btn github-popup-sign-in-btn'
-            onClick={handleSignIn}>
-            <FaGithub className='icon' />
-            <p>Continue with Github</p>
-        </button>
-    )
-}
+  }
+  return (
+    <button
+      className="popup-sign-in-btn github-popup-sign-in-btn"
+      onClick={handleSignIn}
+    >
+      <FaGithub className="icon" />
+      <p>Continue with Github</p>
+    </button>
+  );
+};
 
-const SignInWithGithub = withFirebase(SignInWithGithubBase)
-const SignInWithGoogle = withFirebase(SignInWithGoogleBase)
-const SignInForm = withFirebase(SignInFormBase)
+const SignInWithGithub = withFirebase(SignInWithGithubBase);
+const SignInWithGoogle = withFirebase(SignInWithGoogleBase);
+const SignInForm = withFirebase(SignInFormBase);
 
-export default SignInPage
+export default SignInPage;
--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app"
+import { initializeApp } from "firebase/app";
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -9,13 +9,18 @@ import {
   sendEmailVerification,
   GoogleAuthProvider,
   signInWithPopup,
-  GithubAuthProvider
-}
-  from 'firebase/auth'
+  GithubAuthProvider,
+} from "firebase/auth";
 // Realtime database
-import { getDatabase, onValue, ref, set } from 'firebase/database'
+import { getDatabase, onValue, ref, set } from "firebase/database";
 // Firebase Storage for Download things
-import { getStorage, getDownloadURL, ref as sRef, listAll, uploadBytesResumable } from 'firebase/storage'
+import {
+  getStorage,
+  getDownloadURL,
+  ref as sRef,
+  listAll,
+  uploadBytesResumable,
+} from "firebase/storage";
 
 const config = {
   apiKey: "AIzaSyBBIiIfMcE0q_7Zm596EQYDdJR3VP7qLIE",
@@ -24,157 +29,190 @@ const config = {
   storageBucket: "my-contact-page-89.appspot.com",
   messagingSenderId: "764757300986",
   appId: "1:764757300986:web:eca054fd2a638f16939b43",
-  databaseURL: "https://my-contact-page-89-default-rtdb.asia-southeast1.firebasedatabase.app",
-  storageBucket: 'gs://my-contact-page-89.appspot.com/'
-}
+  databaseURL:
+    "https://my-contact-page-89-default-rtdb.asia-southeast1.firebasedatabase.app",
+  storageBucket: "gs://my-contact-page-89.appspot.com/",
+};
 const actionCodeSettings = {
-  url: 'https://premium-ebook-store.netlify.app/',
+  url: "https://premium-ebook-store.netlify.app/",
   handleCodeInApp: true,
   iOS: {
-    bundleId: 'com.example.ios'
+    bundleId: "com.example.ios",
   },
   android: {
-    packageName: 'com.example.android',
+    packageName: "com.example.android",
     installApp: true,
-    minimumVersion: '12'
+    minimumVersion: "12",
   },
-  dynamicLinkDomain: 'example.page.link'
+  dynamicLinkDomain: "example.page.link",
 };
 
-const app = initializeApp(config)
-const auth = getAuth(app)
-const db = getDatabase(app)
-const storage = getStorage(app)
-const googleProvider = new GoogleAuthProvider()
-const githubProvider = new GithubAuthProvider()
+const app = initializeApp(config);
+const auth = getAuth(app);
+const db = getDatabase(app);
+const storage = getStorage(app);
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
 
 class Firebase {
   constructor() {
-    initializeApp(config)
-    this.auth = auth
+    initializeApp(config);
+    this.auth = auth;
   }
   doCreateUserWithEmailAndPassword = (email, password) =>
-    createUserWithEmailAndPassword(this.auth, email, password)
+    createUserWithEmailAndPassword(this.auth, email, password);
 
   sendEmailVerification = (user) =>
-    sendEmailVerification(user)
-      .then(() => { console.log("Email Verification Sent.....") })
+    sendEmailVerification(user).then(() => {
+      console.log("Email Verification Sent.....");
+    });
 
   doSignInWithEmailAndPassword = (email, password) =>
-    signInWithEmailAndPassword(this.auth, email, password)
+    signInWithEmailAndPassword(this.auth, email, password);
 
   doSendSignInLinkToEmail = (email) => {
     sendSignInLinkToEmail(this.auth, email, actionCodeSettings)
       .then(() => {
-        console.log("Email Link Sent Successfully !")
+        console.log("Email Link Sent Successfully !");
       })
-      .catch(error => {
-        console.log(error.code + " " + error.message)
-      })
-  }
+      .catch((error) => {
+        console.log(error.code + " " + error.message);
+      });
+  };
 
-  doSignOut = () => this.auth.signOut()
+  doSignOut = () => this.auth.signOut();
 
-  doPasswordReset = email => sendPasswordResetEmail(this.auth, email)
-  doPasswordUpdate = password => updatePassword(password)
+  doPasswordReset = (email) => sendPasswordResetEmail(this.auth, email);
+  doPasswordUpdate = (password) => updatePassword(password);
 
   writeDataToDB = (uid, userName, email) => {
-    return set(ref(db, `users/${uid}`), { userName, email })
-  }
+    return set(ref(db, `users/${uid}`), { userName, email });
+  };
 
   writeEBookDataToDB = (title, eBookDownloadUrl, eBookCoverDownloadUrl) => {
-    return set(ref(db, `eBooks/${title}`), { title, eBookDownloadUrl, eBookCoverDownloadUrl })
-  }
+    return set(ref(db, `eBooks/${title}`), {
+      title,
+      eBookDownloadUrl,
+      eBookCoverDownloadUrl,
+    });
+  };
 
   readDataFromDB = (handleUsersObj, path) => {
-    let usersRef = ref(db, path)
-    let listener = onValue(usersRef, (snapshot) => handleUsersObj(snapshot))
-    return listener
-  }
+    let usersRef = ref(db, path);
+    let listener = onValue(usersRef, (snapshot) => handleUsersObj(snapshot));
+    return listener;
+  };
 
   uploadEBookFiles = (file, setProgressLevel) => {
-    let eBookDownloadUrl, eBookCoverDownloadUrl
+    let eBookDownloadUrl, eBookCoverDownloadUrl;
 
-    const eBookStorageRef = sRef(storage, `ebooks/${file.eBookName}`)
-    const eBookCoverStorageRef = sRef(storage, `ebooks-cover/${file.eBookName}-cover`)
+    const eBookStorageRef = sRef(storage, `ebooks/${file.eBookName}`);
+    const eBookCoverStorageRef = sRef(
+      storage,
+      `ebooks-cover/${file.eBookName}-cover`
+    );
 
-    const uploadeBookTask = uploadBytesResumable(eBookStorageRef, file.eBook)
+    const uploadeBookTask = uploadBytesResumable(eBookStorageRef, file.eBook);
     uploadeBookTask.on(
-      'state_changed',
+      "state_changed",
       (snap) => {
-        setProgressLevel(prev => ({ ...prev, ['eBookProgressLevel']: snap.bytesTransferred / snap.totalBytes * 100 }))
+        setProgressLevel((prev) => ({
+          ...prev,
+          ["eBookProgressLevel"]:
+            (snap.bytesTransferred / snap.totalBytes) * 100,
+        }));
       },
       (err) => console.log(err),
       () => {
         getDownloadURL(eBookStorageRef).then((downloadUrl) => {
-          eBookDownloadUrl = downloadUrl
+          eBookDownloadUrl = downloadUrl;
 
           if (eBookCoverDownloadUrl && eBookDownloadUrl) {
-            console.log("my func 1 running.")
-            this.writeEBookDataToDB(file.eBookName, eBookDownloadUrl, eBookCoverDownloadUrl)
-            eBookCoverDownloadUrl = null
-            eBookDownloadUrl = null
-            return true
+            console.log("my func 1 running.");
+            this.writeEBookDataToDB(
+              file.eBookName,
+              eBookDownloadUrl,
+              eBookCoverDownloadUrl
+            );
+            eBookCoverDownloadUrl = null;
+            eBookDownloadUrl = null;
+            return true;
           }
-        })
+        });
       }
-    )
-    const uploadeBookCoverTask = uploadBytesResumable(eBookCoverStorageRef, file.eBookCover)
+    );
+    const uploadeBookCoverTask = uploadBytesResumable(
+      eBookCoverStorageRef,
+      file.eBookCover
+    );
     uploadeBookCoverTask.on(
-      'state_changed',
+      "state_changed",
       (snap) => {
-        console.log("eBook Cover Progress ->" + snap.bytesTransferred / snap.totalBytes * 100)
-        setProgressLevel(prev => ({ ...prev, ['eBookCoverProgressLevel']: snap.bytesTransferred / snap.totalBytes * 100 }))
+        console.log(
+          "eBook Cover Progress ->" +
+            (snap.bytesTransferred / snap.totalBytes) * 100
+        );
+        setProgressLevel((prev) => ({
+          ...prev,
+          ["eBookCoverProgressLevel"]:
+            (snap.bytesTransferred / snap.totalBytes) * 100,
+        }));
       },
-      (err) => { console.log(err) },
+      (err) => {
+        console.log(err);
+      },
       () => {
         getDownloadURL(eBookCoverStorageRef).then((downloadUrl) => {
-          eBookCoverDownloadUrl = downloadUrl
+          eBookCoverDownloadUrl = downloadUrl;
 
           if (eBookCoverDownloadUrl && eBookDownloadUrl) {
-            console.log("my func 2 running.")
-            this.writeEBookDataToDB(file.eBookName, eBookDownloadUrl, eBookCoverDownloadUrl)
-            eBookCoverDownloadUrl = null
-            eBookDownloadUrl = null
-            return true
+            console.log("my func 2 running.");
+            this.writeEBookDataToDB(
+              file.eBookName,
+              eBookDownloadUrl,
+              eBookCoverDownloadUrl
+            );
+            eBookCoverDownloadUrl = null;
+            eBookDownloadUrl = null;
+            return true;
           }
-        })
-      })
-  }
+        });
+      }
+    );
+  };
 
   downloadFile = (downloadAddress, eBookName, setIsDownloading) => {
-    fetch(downloadAddress)
-      .then(response => {
-        response.blob().then(blob => {
-          // Creating new object of PDF file
-          const fileURL = window.URL.createObjectURL(blob);
-          let a = document.createElement('a');
-          a.href = fileURL;
-          a.download = `${eBookName}.pdf`;
-          a.click();
-          setIsDownloading(false)
-        })
-      })
-  }
+    fetch(downloadAddress).then((response) => {
+      response.blob().then((blob) => {
+        // Creating new object of PDF file
+        const fileURL = window.URL.createObjectURL(blob);
+        let a = document.createElement("a");
+        a.href = fileURL;
+        a.download = `${eBookName}.pdf`;
+        a.click();
+        setIsDownloading(false);
+      });
+    });
+  };
 
   getAllBooksList = () => {
-    listAll(sRef(storage, 'ebooks/'))
-      .then(res => {
+    listAll(sRef(storage, "ebooks/"))
+      .then((res) => {
         console.log(res.items[0].name);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
-      })
-  }
+      });
+  };
   signInWithGoogle = () => {
     signInWithPopup(auth, googleProvider)
-      .then(result => console.log('Signin with Google Successful...'))
-      .catch(err => console.log(err))
-  }
+      .then(() => console.log("Signin with google successfull..."))
+      .catch((err) => console.log(err));
+  };
 
   signInWithGithub = async () => {
-    return await signInWithPopup(auth, githubProvider)
-  }
+    return await signInWithPopup(auth, githubProvider);
+  };
 }
 
-export default Firebase
+export default Firebase;
